c47875811c4d2a5526d342e69b7943701fedbb1f
Sebastian Brock <sebastian.brock@outlook.de>
2024-04-17 14:09:33 +0200

    fix public path for th server deployment

--- webapplikation/frontend/th_koeln_barrierefreiheit/vue.config.js
+++ webapplikation/frontend/th_koeln_barrierefreiheit/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/P3_Barrierefreiheit_ECommerce/" : "/",
+  publicPath: "./",
   transpileDependencies: true,
   css: {
     loaderOptions: {
