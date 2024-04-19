5d16844c8be04199e528ae2cc678ad3ecc27da30
Sebastian Brock <sebastian.brock@outlook.de>
2024-04-19 12:37:40 +0200

    fix hljs css colors

--- webapplikation/frontend/th_koeln_barrierefreiheit/src/App.vue
+++ webapplikation/frontend/th_koeln_barrierefreiheit/src/App.vue
@@ -7,4 +7,10 @@
 @import "@/assets/styles/base.scss";
 @import "@/assets/styles/layout.scss";
 @import "@/assets/styles/navHeader.scss";
+
+code[class*=language-], pre[class*=language-] .hljs {
+  background: #2b2b2b !important;
+  color: #f8f8f2 !important;
+  text-shadow: none !important;
+}
 </style>
